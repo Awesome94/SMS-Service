bfbb88e52e9728d5a8ee387a5ee58e0560e54550
awesome94 <david.kironde@andela.com>
2019-08-13 17:03:57 +0300

    add tests skelton

---
+++ tests/test_sms_service.py
@@ -0,0 +1,28 @@
+import unittest
+import json
+import os
+from app import app, nexmo_client, africas_talking_client
+
+
+class TestSMSservice(unittest.TestCase):
+    def test_format_of_recipient_number(self):
+        pass
+
+    def test_default_provider(self):
+        pass
+
+    def test_when_provider_is_specified(self):
+        pass
+
+    def test_messages_are_sent_to_queue(self):
+        pass
+
+    def test_landing_route(self):
+        pass
+
+    def test_all_endpoint_load(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
